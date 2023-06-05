f4afc90972dd7088417d0d46157f6708c52dc713
unknown <utkarshchaudhry14@gmail.com>
2023-06-05 17:17:01 +0530

    step-4

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 
 
+import 'dart:js';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -7,7 +9,7 @@ import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
-import 'dart:developer' as devtools show log;
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
@@ -19,9 +21,10 @@ void main() {
       routes:{
         '/login/':(context)=> const LoginView(),
         '/register/':(context)=>const RegisterView(),
+        '/notes/':(context)=>const NotesView(),
       }
     ),
-    );
+  );
 }
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,6 +1,7 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}):super(key:key);
 
@@ -61,14 +62,18 @@ class _LoginViewState extends State<LoginView> {
                       email: email,
                       password: password,
                       );
-                      print(userCredential);
+                      //devtools.log(userCredential.toString());
+                      //print(userCredential);
+                      Navigator.of(context).pushNamedAndRemoveUntil('/notes/', (route) => false,);
                       } 
                       on FirebaseAuthException catch(e){
                         if(e.code=='user-not-found'){
-                          print('User not found');
+                          devtools.log('User not found');
+                          //print('User not found');
                         }
                         else if(e.code=='wrong-password'){
-                          print('Wrong Password');
+                          devtools.log('Wrong Password');
+                          //print('Wrong Password');
                         }
                       }
                     },
